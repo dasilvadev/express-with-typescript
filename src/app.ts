9876877909fc23d60bac5a6defe953237322981a
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-07-16 15:40:49 -0300

    example: products post route

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import express from 'express';
 const app = express();
 const serverPort = 3000; 
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     return res.send("Hello Express!");
 });
 
+app.post('/products', (req, res) => {
+    console.log(req.body);
+
+    return res.send("New product created");
+});
+
 app.listen(serverPort, () => {
-    console.log(`Server running on port ${serverPort}`);
+    console.log(`Server running on port ${serverPort}.`);
 });
