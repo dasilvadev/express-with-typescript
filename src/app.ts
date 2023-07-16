949fd5dabb8771fc75a0fcc05650300081d4c2d3
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-07-16 16:05:05 -0300

    example: products check all route

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,20 @@ app.post('/products', (req, res) => {
     return res.status(201).send("New product created",);
 });
 
+app.all('/products/check', (req, res) => {
+    console.log("Request method: %s", req.method);
+
+    if (req.method === "GET") {
+        return res.send("Read product!");
+    }
+
+    if (req.method === "POST") {
+        return res.status(201).send("Product created!");
+    }
+
+    return res.status(404).send("Not found!");
+})
+
 app.listen(serverPort, () => {
     console.log(`Server running on port ${serverPort}.`);
 });
